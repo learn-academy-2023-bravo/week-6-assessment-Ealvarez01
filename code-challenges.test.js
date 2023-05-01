0fce10609bcac264730bb0bf6b6775241a1cddaf
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-05-01 00:45:21 -0700

    competed week-6 assessment-finally-no-more

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,33 +14,190 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+//describe(name of function as a string, callback function)
+describe("arrayCaps", () => {
+  
+  // Given function of a array of objects with expected outputs
+  const people = [
+    { name: "ford prefect", occupation: "a hitchhiker" },
+    { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+    { name: "arthur dent", occupation: "a radio employee" }
+    // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+  ]  
+    //it(description of the function as a string, callback function)
+    it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized.", () => {
+
+    //expect(statement with the function call and jest matcher with the expected output) 
+    expect(arrayCaps(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+    })
+})
+
+// FAIL  ./code-challenges.test.js
+// arrayCaps
+//   ✕ takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+
+// ● arrayCaps › takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+
+//   ReferenceError: arrayCaps is not defined
 
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE
+// input: array of objects
+// output: array
+
+// input: [{ name: "ford prefect", occupation: "a hitchhiker" }, { name: "zaphod beeblebrox", occupation: "president of the galaxy" }, { name: "arthur dent", occupation: "a radio employee" }]
+
+// output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+// PROCESS:
+// Create a function named "arrayCaps" takes in an array of objects, represented by an argument assigned by the function, set in parentheses named "array" and return an array with a sentence about each person with their name capitalized.
+// Create a return statement to invoke the argument of "array" (objects).
+// Use the '.map' built-in method, which is run by the argument of the function, for every array element and pass in an argument named "object" (represents the objects in the array) in a set of parentheses, enclosed in another set of parentheses which will only run the function for every number element in the array.
+// To allow us to write a shorter function, use the arrow function syntax (=>).
+// Call on the argument of 'object' appended by a specific object value named ".name" which will run the name of each object key value.
+// Then append the value with the '.split()' built-in method followed by a set of parentheses with an enclosed set of quotations making sure to leave a space in between (a seperator to split between words). This will split the string of words into an array of substrings and return a new array.
+// Then we append the '.map' built-in method to this test to run the function for each key value in the array and pass in an argument named "value" in a set of parentheses, enclosed in another set of parentheses (to keep adding more code), which this test will provide the name value of the object key. 
+// To allow us to continue to write a shorter function, use the arrow function syntax (=>).
+// Call on the name value argument and assign the value, using the assignment operator (=) to run the name value and append it with the '.charAt' built-in method that will return a character at a specified index/position. We will add the index of '0' in a set of parentheses as an argument because the index of the first character is zero. 
+// To change the letter casing of the character index, we will apend the '.toUpperCase' built-in method because we want to change the first character to a capital letter followed by a set of empty parentheses (parameters are not required with this method).
+// Now we want to connect the value to the object key 'occupation by using the addition operator (+) and call of the 'value' argument and append the '.slice()' built-in method, which will start at a selected element in the array so we will pass in the index of '1' as an argument, enclosed by two sets of parentheses (to close out all of the previous logic up to this point) to run this test starting at the second character index. 
+// Next, we will append the '.join()' built-in method to join everything back together in the array and pass in quotation marks (seperator) in a set of parentheses, making sure to leave a space in between to join and space in between words.
+// To add a sentence to the code, us the addition operator (+) to connect the logic to the sentence. 
+// The sentence is called with a string interpolation method to insert different words into the string. We use a set of back-tics syntax (``) define the string, followed by the word 'is' (all the previous logic is only key value names so all we do here is add the key value occupation), a dollar symbol ($) followed by curly braces {}, within the curly braces, call on the initial object key, append the .occupation key value, followed by a period outside of the curly braces to end the sentence, close with the final back-tic and close with the final parentheses. 
+// Returns an array with a sentence about each person with their name capitalized.
+
+  const arrayCaps = (array) => {
+    return array.map((object) => object.name.split(" ").map((value) => value = (value.charAt(0).toUpperCase() + value.slice(1))).join(" ") + ` is ${object.occupation}.`)
+  }
+
+// PASS  ./code-challenges.test.js
+//   arrayCaps
+//     ✓ takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (1 ms)
+
+// Test Suites: 1 passed, 1 total
+
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+//describe(name of function as a string, callback function)
+describe("divisibleByThree", () => {
+
+  // Given function of a mixed data array with expected outputs
+  const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+  // Expected output: [ 2, 0, -1, 0 ]
+  const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+  // Expected output: [ 2, 1, -1 ]
+
+    //it(description of the function as a string, callback function)
+    it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+
+    //expect(statement with the function call and jest matcher with the expected output) 
+    expect(divisibleByThree(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(divisibleByThree(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+//   divisibleByThree
+//     ✕ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3 (1 ms)
+
+//   ● divisibleByThree › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3
+
+//     ReferenceError: divisibleByThree is not defined
 
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE
+// input: two mixed data arrays
+// output: two arrays of the remainders of the numbers divided by three 
+
+// input:[23, "Heyyyy!", 45, -10, 0, "Yo", false]
+// output: [ 2, 0, -1, 0 ]
+
+// input: [5, "Hola", 43, -34, "greetings", true]
+// output: [ 2, 1, -1 ]
+
+// PROCESS:
+// Create a function named "divisibleByThree" that takes in a mixed data array, represented by an argument assigned by the function, set in parentheses named "arrayNumbers" and return an array of only the REMAINDERS of the numbers when divided by 3.
+// Create a return statement to invoke the argument of "arrayNumbers" (numbers).
+// Use the '.filter' built-in method which will create a new array, run thru the array and fill it with only the number elements that passes the test assigned by the function. 
+// To tell the function which value we want to test from the array, we have to pass an argument named "value" in a set of parentheses enclosed in another set of parentheses. 
+// To allow us to write a shorter function, use the arrow function syntax (=>) and use the 'typeof' operator to find the data type we want, then call on the 'value' argument and use the strictly equal operator (===) to return only what we assign the value, then assign the value as a "number" because we only want the numbers returned from the array.
+// Use the '.map' built-in method to call on the function for every array element by passing in an argument named "num" (represents only the numbers) in a set of parentheses, enclosed in another set of parentheses which will only run the function for every number element in the array.
+// We use an arrow syntax (=>) to continue writing a shorter code and we will call on the "num" argument and use the modulo operator (%) which represents the division remainder value and set it the value of three because we want to return the remainders of the numbers when they are divided by three. 
+// Returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+  const divisibleByThree = (arrayNumbers) => {
+    return arrayNumbers.filter((value) => typeof value === "number").map((num) => num % 3)
+  }
+
+// PASS  ./code-challenges.test.js
+// divisibleByThree
+//   ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3
+
+// Test Suites: 1 passed, 1 total
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+//describe(name of function as a string, callback function)
+describe("arrayCubed", () => {
+
+   // Given function of an array of numbers with expected outputs
+  const cubeAndSum1 = [2, 3, 4]
+  // Expected output: 99
+  const cubeAndSum2 = [0, 5, 10]
+  // Expected output: 1125  
+
+  //it(description of the function as a string, callback function)
+  it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+    
+  //expect(statement with the function call and jest matcher with the expected output) 
+  expect(arrayCubed(cubeAndSum1)).toEqual(99)
+  expect(arrayCubed(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+// arrayCubed
+//   ✕ takes in an array of numbers and returns the sum of all the numbers cubed (1 ms)
+
+// ● arrayCubed › takes in an array of numbers and returns the sum of all the numbers cubed
+
+//   ReferenceError: arrayCubed is not defined
 
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE
+// input: two arrays
+// output: two numbers
+
+// input: [2, 3, 4]
+// output: 99
+
+// input: [0, 5, 10]
+// output: 1125
+
+// PROCESS:
+// Create a function named "arrayCubed" that takes in an array of numbers, represented by an argument assigned by the function, set in parentheses named "arrays" and return the sum of all the numbers cubed.
+// Create a return statement to invoke the argument of "arrays" (numbers).
+// Use the '.reduce' built-in method which will reduce the elements in the array and return only the accumulated single number value from the array. 
+// Set the initial value of the function by passing an argument named "sum" (representing the sum of the numbers in the array) and set a value named "number), which represents the number elements in the array that are to be returned.
+// To allow us to write a shorter function, use the arrow function syntax (=>) and call on the sum of the numbers by using the addition operator (+) to produce/connect the sum of the numbers cubed.
+// Pass in an argument that uses the multiplication operator (*) to multiply each number three times, which represents the numbers cubed.
+// Add the index of '0" preceded by a comma (,) and place outside of the math logic to represent the first index number (starting point). 
+// Returns the sum of all the numbers cubed.
+  
+  const arrayCubed = (arrays) => {
+    return arrays.reduce((sum, number) => sum + (number * number * number), 0)
+}
+
+// PASS  ./code-challenges.test.js
+// arrayCubed
+//   ✓ takes in an array of numbers and returns the sum of all the numbers cubed
+
+// Test Suites: 1 passed, 1 total
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1)Purpose is to generate a controller application and class for "BlogPosts" which controls the behavior of the entire code. The name of the controller will follow the PascalCase naming convention. The functionality of the controller creates a class for BlogPosts in the Application controller and this is the main source that all of your code will be inputed. The purpose and functionality of defining a method "index" is to add instances of the class controller to the database to access a list of all the things that is going to interact with the database. It is a controller method that makes an Active Record call that saves and holds the active record queries in the controller.   
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) The purpose is to create an instance variable named "@posts" that belongs to the class controller app "BlogPosts" and calls on the index controller method. The functionality uses the '.all' built-in method that will display all of the data values in the BlogPost database. The instance variable will make an Active Record .all action call by referencing the controller name that will save all of the content from the database as an instance variable. It will hold all of the active record queries in the database. The "end" closes the code logic block, whatever is contained between the define and end lines are specific to that code block.  
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) The purpose is to define a method of a class called "show" that will give us the details of a specific item in the database.    The functionality of creating an instance variable named "@post" for the method is to find and locate certain specific detailed information in the database and make an Active Record .find action call by referencing the controller name. The ".find" built-in method will look thru all of the information in the BlogPost database and display only that one item. The URL params/parameters gets passed as an argument in a set of parentheses which it then passes the id of a blog post as a second argument to get the id value which is set in square brackets, preceded by a colon (:) and both are set to the '.find' built-in method to access only a specific blog id in the database. The "end" closes the code logic block, what ever is contained between the define and end lines are specific to that code block. 
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) The purpose of this is to define a method of the class named "new" to create an instance variable named "@post" to allow access to add or create a new form to the application that will be stored in the database. We can create as many instance variables as we want. The functionality of the method, after defining it, is to call upon the instance variable of the class "@post" and use an assignment operator to call on the Active Record .new action by referencing the controller name to input a new post with the built-in method '.new'. This will allow access to input and store a new Blog Post into the database. The "end" closes the code logic block, what ever is contained between the define and end lines are specific to that code block. Then we will define a new method of the class named "create" to add content to the new blog post form in the database. 
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) This is an instance variable named "@post" for the method create. The functionality of this instance variable will allow access to create and submit a new blog post and set up parameters for what can be accessed when creating a new entry into the database. Therefore, we call on the Active Record .create action to call on a strong params method and pass in a strong params argument named "blog_post_params" and set conditions to allow specific access to information in the database. The strong params method will be listed under the keyword "private" at the end. A conditional stament starts with the word "if", then we call on an instance variable "@post" and use a built-in method named '.valid?' to ask the database to read the new information requested for input and decide whether or not it is valid and meets the requirements that are set in the strong params method. If the new input is valid, we will redirect back to the blog_post page by using the 'redirect_to' method to direct a certain page's URL path (route alias), we will reference the path to return it to the new post that was created by passing the instance variable "@post" as an argument set in parameters by using parentheses and it will add the new entry into the database. The "end" closes the conditional statement and defined method code logic block, what ever is contained between the define and end lines are specific to that code block. Then we will define a new method of the class named "edit" to allow access to edit the blog post.   
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -32,13 +32,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6)The instance variable assigns the class BlogPost and calls on the Active Record .find action to find or locate an item in the database by using the '.find' built-in method. We pass an argument named "params" in a set of parentheses to reference the parameters we want to set and allow access to. Then within the params argument, we pass in a second argument to request the database to only locate and return one item by a specific id to edited and we pass in the URL params 'id' in a set of square brackets and add a colon (:) before the 'id' to get the 'id' value. This pulls the value of 'id' from the params database and returns only the edited blog post with that specific id. The "end" closes the code logic block, whatever is contained between the define and end lines are specific to that code block. Then we want to define a new method of the class named "update" to allow access to update a blog post to be stored in the database. We use the instance variable "@post", reference the controller name and call on the Active Record ''.find' to find and update a blog post using the same functionality and code logic as above for editing. 
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) We want the instance variable named "@post" to to set certain parameters that allow access to update a specific blog post in the database calling on the Active Record .update action and pass in an argument for the strong params named "blog_post_params" in a set of parentheses to limit what can be accessed to update and what the input requirements are to be stored in the database. We will use a conditional statement starting with "if" and will set a condition that requests the database to read and decide if the updated input is valid make an Active Record .valid? action call using the '.valid?' built-in method. If the input matches and is validated by the params requirements set, we will allow the input request to update the blog post, entered into the database and direct it back to the updated blog post page by referencing the URL alias path 'blog_post' and pass in the instance variable "@post" as an argument in a set of parentheses to return to the updated blog post page. The "end" closes the conditional statement and defined method code logic block, whatever is contained between the define and end lines are specific to that code block. Then we define a new method of the class named "destroy" to allow access to destroy or delete a blog post within the database. The instance variable named "@post" will call on and assign the controller class Active Record .find action to find or locate a specific post by using the '.find' built-in method and passing in a params argument in a set of parentheses named "params" and reference a specific 'id' in a set of square brackets and add a colon (:) before the 'id' to get the 'id' value. This pulls the value of "id" from the params database. We set a conditional statement starting with "if" to create a condition that will take the instance variable and ask the database to read and decide if the requested input matches the params requirements. If the input for the blog post is valid we will call on the Active Record .destroy action to delete only the specific blog post with the matching id and remove it from the database. It will not return anything because the blog post has been deleted.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -48,15 +48,14 @@ class BlogPostsController < ApplicationController
   def destroy
     @post = BlogPost.find(params[:id])
     if @post.destroy
-      # ---8)
+      # ---8) This method redirects a user back to the blog_posts index page URL alias if the book is destroyed and is not found in the database. The "end" for the conditional statement and the defined method closes the code logic block, whatever is contained between the define and end lines are specific to that code block.
       redirect_to blog_posts_path
     end
   end
-
-  # ---9)
+  # ---9) When creating a 'strong params' method we label it "private" to limit where the method can be called on. Private is a keyword that restricts the scope of a method. Under this private keyword, there is no chance the strong params can be called on from somewhere else in the program. The private keyword must be placed above the defined params method name "blog_post_params", at the bottom of the controller file and below the controller actions because once it is implemented, everything below it is included in its protection. Strong params control the information allowed into a database. Strong params sets restrictions on the controller methods. Strong params can only be passed into the show, create, edit, update and delete methods as arguments.  
   private
   def blog_post_params
-    # ---10)
+    # ---10) This defined strong params method is named "blog_post_params". To set certan limitations and requirement in the strong params we use a strong params built-in method named '.require' that sets up certain requirements that must be met in order to allow access to the database. The strong params method name (minus the word 'params') is used to refefence the controller and it must always match the method name, is preceded with a colon (:) and passed as an argument in a set of parentheses. Then the params will permit access to the database contents of the "title" and "content" only by using the strong params'.permit' built-in method which sets the permissions. Then we pass in the title and content columns as aruguments in a set of parentheses, preceded by a colon (:) which will set required pemissions to only only allow access to read, create, update, edit or delete the title or content in the database. Anything not listed in the strong params will not be allowed access the database. The "end" for the defined strong params method closes the code logic block, whatever is contained between the define and end lines are specific to that code block. The "end" for the class controller closes all of the code logic contained within the database controller application.  
     params.require(:blog_post).permit(:title, :content)
   end
 end
